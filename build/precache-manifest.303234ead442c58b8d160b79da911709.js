5ab2468808b17f1819bc2261d0c429622a185a34
TigistBekele <ttbtigist2020@gmail.com>
2021-02-22 17:03:26 -0600

    Project added

---
+++ build/precache-manifest.303234ead442c58b8d160b79da911709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de089970de10a1605c1d14fcb0c6f405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8e3991b8d7ef7e202e4",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e3991b8d7ef7e202e4",
+    "url": "/static/js/main.bb4fbe6f.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
